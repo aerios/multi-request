02b16a5a52aec520246b74d1f339793ba13d61e3
aerios <hari@nolimitid.com>
2016-07-22 09:13:32 +0000

    update mechanism

--- libs/Engine.js
+++ libs/Engine.js
@@ -1,174 +1,80 @@
 var ThrottleEngine = require("throttle-exec");
 var underscore = require("underscore")
 var MyProcess = require('mprocess');
-var Promise = require("bluebird")
+var Promise = require("when/es6-shim/Promise")
 var uuidLength = 10;
-var cpuCount = require("os").cpus().length;
+var MaxProcessCount = require("os").cpus().length * 2;
 var uuidSeed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 var Engine = {};
-var Mailbox = {};
-var ProcessLetterMap = {};
+var MailboxCount = {};
 var ProcessList = [];
 var currentProcess = 0;
-var queuedProcess = 0;
-var cpuLoad = 2;
-var processCount = 0;
-var maxProcessCount = cpuLoad * cpuCount
-// mailbox functionality
+var outstandingRequest = 0
 
-function upProcess(){
-	queuedProcess += 1
-}
-
-function downProcess(){
-	queuedProcess -= 1
-}
-
-function generateUUID(){
-	var str = "";
-	for(var i = 0;i<uuidLength;i++){
-		str += uuidSeed.charAt(Math.random() * (uuidSeed.length-1));
-	}
-	return str;
-}
-
-
-function createMessage(data){
-	return {
-		id:generateUUID(),
-		message:data
-	}
-}
-
-function putMailbox(promise,resolver,rejecter,letter){
-	var id = letter.id;
-	Mailbox[id] = {
-		promise:promise,
-		resolver:resolver,
-		rejecter:rejecter,
-		letter:letter
-	}
-}
-
-function processReply(err,letter){
-	var id = letter.id;
-	var data = letter.message;
-	var isError = letter.is_error;
-	var record = Mailbox[id];	
-	if(Mailbox[id]){
-		if(!err && !isError){
-			record.resolver(data)	
-		}else{
-			record.rejecter(err || new Error(data));
-		}		
-		Mailbox[id] = null;
-		record = null;
-	}
-	downProcess();
-}
-
-function deliverDeadLetter(procIndex){
-	var letters = ProcessLetterMap[procIndex].slice(0);
-	ProcessLetterMap[procIndex] = []
-	letters.forEach(function(letter){
-		processReply(new Error("Worker died while processing request"),letter);
+function getSmallestMailboxCount(){
+	var sorted = underscore.sortBy(MailboxCount,function(val){
+		return val.count
 	})
+	return sorted[0].id;
 }
 
-// end mailbox functionality
+function upMailBoxCount(id){
+	MailboxCount[id].count = MailboxCount[id].count + 1
+	outstandingRequest += 1
+}
 
+function downMailboxCount(id){
+	if(MailboxCount[id]){
+		MailboxCount[id].count = MailboxCount[id].count - 1
+		outstandingRequest -= 1
+	}
+}
 
 function createProcess(procIndex){
-	var proc = new MyProcess("node",[__dirname+"/Worker.js"],MyProcess.FORK);	
-	proc.run();
-	proc.getProcess().on("message",function(letter){
-		processReply(null,letter);
-	})
-	ProcessList[procIndex] = proc;
-	ProcessLetterMap[procIndex] = [];
-	proc.done(function(){		
-		deliverDeadLetter(procIndex)
+	MailboxCount[procIndex] = {id:procIndex,count:0};
+	var proc = new MyProcess("node",[__dirname+"/RequestWorker.js"],MyProcess.FORK);	
+	proc.run();	
+	proc.done(function(){
 		createProcess(procIndex);
-	})	
+	})
+	ProcessList[procIndex] = proc
 	return proc;
 }
 
-function sendLetter(letter){	
-	var proc = ProcessList[currentProcess];	
-	ProcessLetterMap[currentProcess].push(letter);
-	proc.getProcess().send(letter);
-	currentProcess += 1;
-	if(currentProcess >= ProcessList.length){
-		currentProcess = 0;
-	}
-}
-
 function init(){
-	for(var i = 0;i< maxProcessCount * cpuLoad;i++){
-		(function(procIndex){
+	for(var i = 0;i< MailboxCount;i++){
+		(function(procIndex){			
 			var proc = createProcess(procIndex);			
 		})(i)		
 	}
 }
 
-function spawn(){
-	createProcess(processCount);
-	processCount += 1;
+function tryToSpawnWorker(){
+	if(getProcessCount() < MaxProcessCount && outstandingRequest >= underscore.size(MailboxCount)){
+		createProcess(underscore.size(MailboxCount))
+	}
 }
 
-function throttledSpawn(){
-
-	if((processCount * 2) < queuedProcess && processCount < maxProcessCount){
-		spawn();
-	}
-	// console.log((processCount * 2) ,"<", queuedProcess ,"&&", processCount ,"<", maxProcessCount)
-	// console.log("Worker count :",processCount,"Queued Request :",queuedProcess)
-}
-
-function doRequest(){
-	upProcess();
-	var rawData = underscore.toArray(arguments);
-	//strip any function from data
-	data = underscore.filter(rawData,function(i){
-		return typeof i != "function";
+function doRequest(){		
+	tryToSpawnWorker()
+	var currentProcess = getSmallestMailboxCount()
+	upMailBoxCount(currentProcess)
+	var proc = ProcessList[currentProcess];
+	var data = underscore.toArray(arguments);	
+	// console.log("Worker no",currentProcess,"is selected!")
+	var pr = proc.send(data).then(function(result){
+		downMailboxCount(currentProcess)
+		return result;
+	}).catch(function(reason){
+		downMailboxCount(currentProcess)
+		return reason;
 	})
-	var baseFunc = underscore.filter(rawData,function(i){
-		return typeof i == "function"
-	})[0]
-	var letter = createMessage(data);
-	var id = letter.id;
-	var pr = new Promise(function(resolve,reject){
-		putMailbox(pr,resolve,reject,letter);		
-	})
-	var idTimeout = setTimeout(function(){
-		processReply(new Error("Timeout detected"),letter);
-	},120000)
-	if(baseFunc){
-		pr.then(function(r){
-			baseFunc(null,r.response,r.body)
-			return r;
-		}).catch(function(reason){
-			baseFunc(reason);
-		})
-	}
-	throttledSpawn()
-	sendLetter(letter);
 	return pr
 }
-
-// process utility
 
 function getProcessCount(){
 	return ProcessList.length;
 }
-
-function changeCpuLoad(i){
-	cpuLoad = i;
-	maxProcessCount = i * cpuCount;
-}
-
 Engine = doRequest
 Engine.getWorkerCount = getProcessCount;
-Engine.changeCpuLoad = changeCpuLoad;
-Engine.getCpuLoad = function(){return cpuLoad}
 module.exports = Engine;
